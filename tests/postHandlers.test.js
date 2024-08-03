4267c6d67e074b284e429f8ed1aa6197865e29be
Bsamboy74 <bsamboy74@gmail.com>
2024-08-03 19:00:55 -0400

    Add files via upload

--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -1,20 +1,36 @@
-// eslint-disable-next-line no-undef
 const config = require('../config');
 
 const requestBody = {
-    // put your body here
-}
+  "name": "Bananas",
+  "cardID": 8
+};
 
-test('', async () => {
-    try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
-			method: 'POST',
-			headers: {
-			'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(requestBody)
-		});
-	} catch (error) {
-		console.error(error);
-	}
+test('Response body should contain "Bananas" or "For the situation...', async () => {
+  let responseStatus;
+  let actualResponseBody;
+  try {
+    const response = await fetch(`${config.API_URL}/api/v1/kits`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody)
+    });
+
+    responseStatus = response.status;
+    actualResponseBody = await response.json();
+
+    
+  } catch (error) {
+    console.error('Error occurred:', error);
+  }
+
+  // Ensure the status is 201 (Created)
+  expect(responseStatus).toBe(201);
+
+  // Ensure the response body contains the correct data
+  expect(actualResponseBody).toHaveProperty('name', 'Bananas');
+  expect(actualResponseBody).toHaveProperty('card');
+  expect(actualResponseBody.card).toHaveProperty('id', 8);
+  expect(actualResponseBody.card).toHaveProperty('name', 'For the situation');
 });
